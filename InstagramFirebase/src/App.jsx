cefc215ec7ea19625e4919abf113bcb321b3dca6
Dambar Sing <programmer.gshi18@gmail.com>
2025-02-14 17:53:23 +0545

    Pushing

---
+++ InstagramFirebase/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="container w-[90vw] h-[90vh] bg-[#fff]">
+      App
+    </div>
+    </>
+  )
+}
+
+export default App
